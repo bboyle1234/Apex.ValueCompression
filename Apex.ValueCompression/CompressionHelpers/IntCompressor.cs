03d730e434eff7e4ab00bdcef08f400b493fe141
Benjamin Boyle <bboyle1234@gmail.com>
2019-05-16 23:35:59 -0400

    IntCompressor now uses zigzag compression

--- Apex.ValueCompression/CompressionHelpers/IntCompressor.cs
+++ Apex.ValueCompression/CompressionHelpers/IntCompressor.cs
@@ -5,45 +5,13 @@ using static System.Math;
 namespace Apex.ValueCompression {
     public static class IntCompressor {
 
-        const int FIVE_BIT_DATA_MASK = 0x1F;    // 0001 1111
-        const int MORE_MASK = 0x80;             // 1000 0000
-        const int SIGN_MASK = 0x40;             // 0100 0000
-        const int MIN_VALUE_MASK = 0x20;        // 0010 0000
-
         public static void WriteCompressedInt(this IWriteBytes stream, int value) {
-
-            /// Because the twos-complement representation of int.MinValue cannot be negated.
-            if (value == int.MinValue) {
-                stream.WriteByte(MIN_VALUE_MASK);
-                return;
-            }
-
-            var isPositive = value >= 0;
-            value = Abs(value);
-
-            if (value <= FIVE_BIT_DATA_MASK) {
-                stream.WriteByte((byte)(value | (isPositive ? 0 : SIGN_MASK)));
-                return;
-            }
-
-            stream.WriteByte((byte)((value & FIVE_BIT_DATA_MASK) | (isPositive ? 0 : SIGN_MASK) | MORE_MASK));
-            value >>= 5;
-            stream.WriteCompressedUInt((uint)value);
+            stream.WriteCompressedUInt((uint)((value << 1) ^ (value >> 31)));
         }
 
         public static int ReadCompressedInt(this IReadBytes input) {
-
-            int firstByte = input.ReadByte();
-            if ((firstByte & MIN_VALUE_MASK) > 0) return int.MinValue;
-
-            var result = firstByte & FIVE_BIT_DATA_MASK;
-
-            if (firstByte < MORE_MASK) {
-                return ((firstByte & SIGN_MASK) == 0) ? result : -result;
-            }
-
-            result += (int)input.ReadCompressedUInt() << 5;
-            return ((firstByte & SIGN_MASK) == 0) ? result : -result;
+            var value = input.ReadCompressedUInt();
+            return (int)(uint)((value >> 1) ^ (-(value & 1)));
         }
 
         public static void WriteCompressedNullableInt(this IWriteBytes stream, int? value) {
